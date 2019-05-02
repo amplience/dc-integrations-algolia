f49c68c387b6a984d8c942ee60c124f4b06dd239
Simon Mudd <smudd@amnplience.com>
2019-05-02 15:38:25 +0100

    feat(http web server): Created an intial application
    Installed libraries for http server and wrote a hello world endpoint for initialisation

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req: express.Request, res: express.Response) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
